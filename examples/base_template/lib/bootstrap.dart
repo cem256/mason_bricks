a8bf4024e884ef60fda03abf720a3905e66b6d7e
cem256 <cemo99@gmail.com>
2024-11-15 15:21:50 +0300

    add app bloc observer to base_template

--- examples/base_template/lib/bootstrap.dart
+++ examples/base_template/lib/bootstrap.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:base_template/app/bloc/app_bloc_observer.dart';
 import 'package:base_template/app/environment/app_environment.dart';
 import 'package:base_template/app/l10n/generated/strings.g.dart';
 import 'package:base_template/core/utils/device_info/device_info_utils.dart';
 import 'package:base_template/core/utils/logger/logger_utils.dart';
 import 'package:base_template/core/utils/package_info/package_info_utils.dart';
 import 'package:base_template/locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<void> bootstrap({required FutureOr<Widget> Function() builder, required AppEnvironment environment}) async {
   FlutterError.onError = (details) {
@@ -15,6 +17,11 @@ Future<void> bootstrap({required FutureOr<Widget> Function() builder, required A
   await runZonedGuarded<Future<void>>(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
+      // Initialize Bloc Observer
+      Bloc.observer = AppBlocObserver(
+        logEvents: false,
+        logTransitions: false,
+      );
       // Initialize Locator and Utils
       await Future.wait([
         Locator.locateServices(environment: environment),
