936f329c4b087680c053f7c5a6f87c66088f33d4
cem256 <cemo99@gmail.com>
2025-01-26 17:50:02 +0300

    base_template v0.2.0, upgrade dependencies, add flutter_gen for asset generation

--- bricks/base_template/hooks/post_gen.dart
+++ bricks/base_template/hooks/post_gen.dart
@@ -30,6 +30,7 @@ Future<void> run(HookContext context) async {
   await _runFlutterPubGet(context, projectName);
   await _runL10nScript(context, projectName);
   await _runBuildRunnerScript(context, projectName);
+  await _runFlutterGenScript(context, projectName);
 }
 
 Future<void> _copyFiles(
@@ -92,6 +93,20 @@ Future<void> _runL10nScript(HookContext context, String projectName) async {
     l10nProgress.complete("L10n script successfully executed!");
   } else {
     l10nProgress.complete("An error occurred on l10n script ${result.stderr.toString()}");
+    exit(exitCode);
+  }
+}
+
+Future<void> _runFlutterGenScript(HookContext context, String projectName) async {
+  final flutterGenProgress = context.logger.progress("Running flutter gen script");
+  final result = await Process.start("sh", ["scripts/flutter_gen.sh"], workingDirectory: projectName);
+
+  final exitCode = await result.exitCode;
+
+  if (exitCode == 0) {
+    flutterGenProgress.complete("Flutter gen script successfully executed!");
+  } else {
+    flutterGenProgress.complete("An error occurred on flutter gen script ${result.stderr.toString()}");
     exit(exitCode);
   }
 }
--- examples/base_template/lib/feature/home/view/home_view.dart
+++ examples/base_template/lib/feature/home/view/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:base_template/app/generated/assets.gen.dart';
 import 'package:base_template/app/l10n/cubit/l10n_cubit.dart';
 import 'package:base_template/app/l10n/extensions/l10n_extensions.dart';
 import 'package:base_template/app/l10n/generated/strings.g.dart';
@@ -14,42 +15,63 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: UIKitText.titleLarge(
-          context,
-          context.l10n.hello,
+      appBar: const _HomeAppBar(),
+      body: Padding(
+        padding: const UIKitPadding.all16(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Assets.icons.flutter.image(),
+          ],
         ),
-        actions: [
-          BlocBuilder<ThemeCubit, ThemeState>(
-            builder: (context, themeState) {
-              return Switch(
-                value: themeState.themeMode == ThemeMode.dark,
-                onChanged: (value) => context.read<ThemeCubit>().setThemeMode(
-                      value ? ThemeMode.dark : ThemeMode.light,
-                    ),
-              );
-            },
-          ),
-          const UIKitHGap.v16(),
-          BlocBuilder<L10nCubit, L10nState>(
-            builder: (context, state) {
-              return DropdownButton<AppLocale>(
-                value: state.locale,
-                items: AppLocale.values
-                    .map(
-                      (e) => DropdownMenuItem<AppLocale>(
-                        value: e,
-                        child: Text(e.name.toUpperCase()),
-                      ),
-                    )
-                    .toList(),
-                onChanged: (value) => context.read<L10nCubit>().setLocale(value!),
-              );
-            },
-          ),
-          const UIKitHGap.v8(),
-        ],
       ),
     );
   }
+}
+
+class _HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const _HomeAppBar();
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: UIKitText.titleLarge(
+        context,
+        context.l10n.hello,
+      ),
+      actions: [
+        BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, themeState) {
+            return Switch(
+              value: themeState.themeMode == ThemeMode.dark,
+              onChanged: (value) => context.read<ThemeCubit>().setThemeMode(
+                    value ? ThemeMode.dark : ThemeMode.light,
+                  ),
+            );
+          },
+        ),
+        const UIKitHGap.v16(),
+        BlocBuilder<L10nCubit, L10nState>(
+          builder: (context, state) {
+            return DropdownButton<AppLocale>(
+              value: state.locale,
+              items: AppLocale.values
+                  .map(
+                    (e) => DropdownMenuItem<AppLocale>(
+                      value: e,
+                      child: Text(e.name.toUpperCase()),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (value) => context.read<L10nCubit>().setLocale(value!),
+            );
+          },
+        ),
+        const UIKitHGap.v8(),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
